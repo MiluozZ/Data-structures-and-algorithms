3eeebca995acaf5db456eb588f466fa8f6eb1737
luozx <604679490@qq.com>
2023-02-21 22:34:28 +0800

    冒泡算法

---
+++ src/com/hanshunping/BubbleSort.java
@@ -0,0 +1,38 @@
+package com.hanshunping;
+
+import java.util.Arrays;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = IntStream.rangeClosed(0, 10).map(x -> {
+            Random random = new Random();
+            return random.nextInt(100);
+        }).toArray();
+        System.out.println(Arrays.toString(arr));
+
+        bubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (j > arr.length) {
+                    return;
+                }
+                if (arr[j+1] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
